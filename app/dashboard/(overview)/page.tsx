fb88edfdc6ab6f0b345309f1c16b16846abc1325
Mayque Oliveira <mayque.731@gmail.com>
2025-02-18 18:05:44 -0400

    starting debounce

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -21,17 +21,17 @@ export default async function Page() {
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        <Suspense>
+        <Suspense fallback={<CardsSkeleton />}>
           <CardWrapper/>
         </Suspense>
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         <Suspense fallback={<RevenueChartSkeleton />}>
-          <RevenueChartSkeleton />
+          <RevenueChart />
         </Suspense>
         {/* <RevenueChart revenue={revenue}/> */}
         <Suspense fallback={<LatestInvoicesSkeleton />}>
-          <LatestInvoicesSkeleton />
+          <LatestInvoices />
         </Suspense>
         {/* <LatestInvoices latestInvoices={latestInvoices} /> */}
       </div>
